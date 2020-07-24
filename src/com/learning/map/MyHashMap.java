54d92d78886b49205c6a925215f74b9c34feef57
MyTetris <2624947117@qq.com>
2020-07-24 19:39:56 +0800

    'ceshi'

--- src/com/learning/map/MyHashMap.java
+++ src/com/learning/map/MyHashMap.java
@@ -2,13 +2,22 @@ package com.learning.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyHashMap {
-    public static void main(String[] args) {
-        Map<String,Integer> map = new HashMap<>();
-        map.put("aa",1);
-        map.put("aa",2);
-        System.out.println(map.get("aa"));
 
+    public static void main(String[] args) {
+        AtomicInteger count = new AtomicInteger(0);
+        Map<Integer,Integer> testMap = new HashMap<>();
+
+        for (int i = 0; i <1000 ; i++) {
+            new Thread(()->{
+                for (int j = 0; j <100000 ; j++) {
+                    testMap.put(count.get(), count.get());
+                    count.incrementAndGet();
+                }
+            }).start();
+        }
+        System.out.println(count.get());
     }
 }
