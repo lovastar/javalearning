7934d4230b9241c348ef4b17e38ecb8da255294b
MyTetris <2624947117@qq.com>
2020-08-01 10:34:53 +0800

    '解决冲突'

--- src/com/learning/map/MyHashMap.java
+++ src/com/learning/map/MyHashMap.java
@@ -6,6 +6,56 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyHashMap {
 
+    class Node{
+        private Object k ;
+        private Object v ;
+        private Node next ;
+        private int hash ;
+
+        public Object getK() {
+            return k;
+        }
+
+        public void setK(Object k) {
+            this.k = k;
+        }
+
+        public Object getV() {
+            return v;
+        }
+
+        public void setV(Object v) {
+            this.v = v;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public int getHash() {
+            return hash;
+        }
+
+        public void setHash(int hash) {
+            this.hash = hash;
+        }
+    }
+    private Node table[];
+
+    public void myPut(Object key,Object value){
+        if(table==null || table.length==0){
+            table = new Node[16];
+        }
+    }
+
+
+
+
+
     public static void main(String[] args) {
 
         AtomicInteger count = new AtomicInteger(0);
